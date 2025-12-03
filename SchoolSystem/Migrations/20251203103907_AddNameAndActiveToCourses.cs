77ae579ea77ad5b3e41403ab74c4df86a204a5f7
exikoz <hussein.hasnawy@gmail.com>
2025-12-03 11:43:22 +0100

    Sql script for adding name and active columns to courses table, created a migrations with the two columns in side the model Course so EF core can still track our history, but i removed all code in the up method and kept the down method code

---
+++ SchoolSystem/Migrations/20251203103907_AddNameAndActiveToCourses.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SchoolSystem.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddNameAndActiveToCourses : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            // We leave this empty because we already ran the SQL manually in SSMS to satisfy the requirement for data first.
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Active",
+                table: "Courses");
+
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "Courses");
+        }
+    }
+}
--- SchoolSystem/Models/Course.cs
+++ SchoolSystem/Models/Course.cs
@@ -5,13 +5,14 @@ namespace SchoolSystemDB.Models
 {
     public class Course
     {
-        //public string Name { get; set; }
-        //public bool Active { get; set; }
         [Key]
         public int CourseId { get; set; }
         public string Schedule { get; set; } = string.Empty;
         public  DateTime CourseStartDate { get; set; }
         public DateTime CourseEndDate { get; set; }
+
+        public string Name { get; set; } = string.Empty;
+        public bool Active { get; set; }
 
         public virtual ICollection<Enrolls> Enrolls { get; set; } = new List<Enrolls>();
     }
