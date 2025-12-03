c15378b3cec9cb5781665a5173079c2921b8a962
skvortsov-ivan <ivan.skvortsov@chasacademy.se>
2025-12-03 13:10:39 +0100

    Three separate classes have been created for the menu logic. LogInMenu has the logic which lets the user both exit the program and enter the main menu. MainMenu lets the user access to the system's methods.  DisplayAndUseMenu is a helping method that includes logic for the user interface.

---
+++ SchoolSystem/ConsoleUI/LogInMenu.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolSystem.ConsoleUI
+{
+    public class LogInMenu
+    {
+        public static void LogIn()
+        {
+            // Admin log in menu options 
+            List<string> menuOptions = new List<string>
+            {
+                "Log in"
+            };
+
+            while (true)
+            {
+                int? userInput = DisplayMenu.DisplayAndUseMenu(menuOptions, "Welcome to the school administration", true);
+                switch (userInput)
+                {
+                    // Admin logs in
+                    case 1:
+                        MainMenu.UseMainMenu();
+                        break;
+
+                    // Admin exits school system
+                    case 0:
+                        Console.WriteLine("\nThank you for using School System!");
+                        return;
+                }
+            }
+        }
+    }
+}
