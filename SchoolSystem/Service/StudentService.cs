95268f30fd45f65008f8fa3024a1d23cf62ddef1
Liza Hjortling <lizhj90@outlook.com>
2025-12-04 11:15:43 +0100

    CrudServices for teacher,student,course,classroom.

---
+++ SchoolSystem/Service/StudentService.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SchoolSystem.data;
+using SchoolSystemDB.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolSystem.Service
+{
+
+    public class StudentService
+    {
+        private readonly SchoolContext _context;
+
+        public StudentService(SchoolContext context)
+        {
+            _context = context;
+        }
+
+        public Student CreateStudent(Student student)
+        {
+            _context.Students.Add(student);
+            _context.SaveChanges();
+            return student;
+        }
+        public List<Student> GetAllStudents()
+        {
+            return _context.Students.ToList();
+        }
+        public Student? GetStudentById(int id)
+        {
+            return _context.Students.Find(id);
+        }
+        public Student? UpdateStudent(int id, Student updatedStudent)
+        {
+            var student = _context.Students.Find(id);
+
+            if (student == null)
+                return null;
+
+            student.FullName = updatedStudent.FullName;
+            _context.SaveChanges();
+            return student;
+        }
+        public bool DeleteStudent(int id)
+        {
+            var student = _context.Students.Find(id);
+
+            if (student == null)
+                return false;
+
+            _context.Students.Remove(student);
+            _context.SaveChanges();
+            return true;
+        }
+
+
+    }
+
+}
+
+
+
---
+++ SchoolSystem/Service/TeacherService.cs
@@ -0,0 +1,58 @@
+﻿using SchoolSystem.data;
+using SchoolSystemDB.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolSystem.Service
+{
+    public class TeacherService
+    {
+        private readonly SchoolContext _context;
+
+        public TeacherService(SchoolContext context)
+        {
+            _context = context;
+
+        }
+        public Teacher CreateTeacher(Teacher teacher)
+        {
+            _context.Teachers.Add(teacher);
+            _context.SaveChanges();
+            return teacher;
+        }
+        public List<Teacher> GetAllTeachers()
+        {
+            return _context.Teachers.ToList();
+        }
+        public Teacher? GetTeacherById(int id)
+        {
+            return _context.Teachers.Find(id);
+        }
+        public Teacher? UpdateTeacher(int id, Teacher updatedTeacher)
+        {
+            var teacher = _context.Teachers.Find(id);
+
+            if (teacher == null)
+                return null;
+
+            teacher.FullName = updatedTeacher.FullName;
+            teacher.TeachingSubject = updatedTeacher.TeachingSubject;
+            _context.SaveChanges();
+            return teacher;
+        }
+        public bool DeleteTeacher(int id)
+        {
+            var teacher = _context.Teachers.Find(id);
+
+            if (teacher == null)
+                return false;
+
+            _context.Teachers.Remove(teacher);
+            _context.SaveChanges();
+            return true;
+        }
+    }
+}
